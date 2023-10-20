16296d5afc14c3132ee5a7101bb0eb6864dcd8f2
Tomáš Dámek <tomas.damek@tietoevry.com>
2023-10-20 12:02:11 +0200

    feat(#20): CustomTheme implementation

--- src/admin/pages/UsersPage/UsersPage.tsx
+++ src/admin/pages/UsersPage/UsersPage.tsx
@@ -1,17 +1,27 @@
+import { Box, Text, useTheme } from "@chakra-ui/react";
+
 import { useCurrentUserQuery } from "../../../common/security/hooks/queries/useCurrentUserQuery";
 import { useKeycloakParsedToken } from "../../../common/security/hooks/queries/useKeycloakParsedToken";
 
 const UsersPage = () => {
+  const theme = useTheme();
   const { data: parsedToken } = useKeycloakParsedToken();
 
   const { data: currentUser } = useCurrentUserQuery();
 
   return (
-    <>
-      <h1>UsersPage</h1>
+    <Box
+      sx={{
+        backgroundColor: theme.palette.primary.main,
+        borderRadius: theme.borderRadius.element,
+      }}
+    >
+      <Text size="h1" variant="white">
+        UsersPage
+      </Text>
       {JSON.stringify(parsedToken, null, 2)}
       {JSON.stringify(currentUser, null, 2)}
-    </>
+    </Box>
   );
 };
 
